eaa5a73300c2ba04b3d561d56081dc9d7f898ff8
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-04-25 16:45:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/loginregistrationproject/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.loginregistrationproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        val signUpLink = findViewById<TextView>(R.id.tvSignUpLink)
+        val intent = Intent(this,Registration::class.java)
+
+        signUpLink.setOnClickListener {
+            Toast.makeText(this,"Link clicked", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/loginregistrationproject/Registration.kt
@@ -0,0 +1,31 @@
+package com.example.loginregistrationproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_registration)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+
+        val backButton = findViewById<ImageView>(R.id.ivBackButton)
+        val intent = Intent(this, MainActivity::class.java) //explicit intent
+
+        backButton.setOnClickListener{
+            startActivity(intent)
+
+        }
+    }
+}
