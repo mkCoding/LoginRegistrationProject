57f174fca87b778ff06e2b0e2dffafc65422ff13
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local.meter>
2024-04-28 17:57:30 -0400

    updated code for registration email and password validation

--- app/src/main/java/com/example/loginregistrationproject/MainActivity.kt
+++ app/src/main/java/com/example/loginregistrationproject/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.loginregistrationproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +24,47 @@ class MainActivity : AppCompatActivity() {
 //            insets
 //        }
 
-        val signUpLink = findViewById<TextView>(R.id.tvSignUpLink)
-        val intent = Intent(this,Registration::class.java)
 
+        //Login check
+        val email = findViewById<EditText>(R.id.etEmailAddress)
+        val password = findViewById<EditText>(R.id.etPassword)
+        findViewById<ExtendedFloatingActionButton>(R.id.btnLogin).setOnClickListener {
+            // write logic to handle the button
+            val submittedEmail = email.text.toString()
+            val submittedPassword = password.text.toString()
+
+            /*
+             TODO: Create the logic to verify user login details,
+              EMAIL should be valid,
+              Password must have at least 8 length including small/capital letters with digit and special characters
+             */
+            var isValidEmail = false //flag to determine if email is valid or not
+            var isValidPassword = false //flag to determine if password is valid or not
+
+            val pattern = Patterns.EMAIL_ADDRESS //a pattern object which references Email address pattern
+
+            //assign true/false after email and password is validated
+            isValidEmail = pattern.matcher(submittedEmail).matches()?: false //validate email
+            isValidPassword = submittedPassword.matches("[0-9a-zA-Z\\W]{8,}".toRegex()) //validate password
+
+            /*
+            Created when conditional statement
+            - if both email and password is true Print Login Successful
+            - if both email and password is false Show Message "Login Unsuccessful
+             */
+            when (Pair(isValidEmail, isValidPassword)){
+                Pair(true, true) -> Toast.makeText(applicationContext,"Login Successful",Toast.LENGTH_SHORT).show() //both username and password is correct
+                else -> Toast.makeText(applicationContext,"Login Unsuccessful",Toast.LENGTH_SHORT).show() //username or password is incorrect
+            }
+        }
+
+        //Add sign up link functionality
+        val signUpLink = findViewById<TextView>(R.id.tvSignUpLink)
+        val intent = Intent(this, Registration::class.java) //explicit intent
+
+        //when signup is clicked navigate to Registration Activity
         signUpLink.setOnClickListener {
+
             Toast.makeText(this,"Link clicked", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         }
--- app/src/main/java/com/example/loginregistrationproject/Registration.kt
+++ app/src/main/java/com/example/loginregistrationproject/Registration.kt
@@ -2,11 +2,16 @@ package com.example.loginregistrationproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +24,101 @@ class Registration : AppCompatActivity() {
 //            insets
 //        }
 
-
+        //Back Button Functionality
         val backButton = findViewById<ImageView>(R.id.ivBackButton)
         val intent = Intent(this, MainActivity::class.java) //explicit intent
 
-        backButton.setOnClickListener{
+        backButton.setOnClickListener {
             startActivity(intent)
-
         }
+
+
+        val registrationButton = findViewById<ExtendedFloatingActionButton>(R.id.btnSignUpButton)
+        registrationButton.setOnClickListener {
+            /*
+            when button is clicked:
+            - Verify the email and password is valid
+            - Also verify the confirm password matches the password
+             */
+
+            //Registration check
+            val registerEmail = findViewById<EditText>(R.id.etRegisterEmail)
+            val registerPassword = findViewById<EditText>(R.id.etRegistrationPassword)
+            val registerConfirmPassword = findViewById<EditText>(R.id.etRegistrationConfirmPassword)
+
+            //Convert email, password and confirm password to strings
+            val registerEmailString = registerEmail.text.toString()
+            val registerPasswordString = registerPassword.text.toString()
+            val registerConfirmPasswordString = registerConfirmPassword.text.toString()
+
+            //Validate registration email and password
+            var isValidRegistrationEmail =
+                Patterns.EMAIL_ADDRESS.matcher(registerEmailString).matches() ?: false
+
+            var passRegex =
+                "^.*(?=.{8,})(?=.+[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=]).*\$"
+
+
+            var isValidRegisterPassword =
+                registerPasswordString.matches(passRegex.toRegex()) ?: false
+            var isMatchingPasswordAndConfirmPassword =
+                registerPasswordString == registerConfirmPasswordString ?: false //confirms if both password and confirm password matches
+
+
+
+            if (isValidRegistrationEmail && isValidRegisterPassword && isMatchingPasswordAndConfirmPassword) {
+                Toast.makeText(this, "Registration Successful", Toast.LENGTH_LONG).show()
+            } else if (!isValidRegistrationEmail && isValidRegisterPassword && isMatchingPasswordAndConfirmPassword) {
+                //invalid email only
+                Toast.makeText(this, "Invalid email address", Toast.LENGTH_LONG).show()
+
+            } else if (!isValidRegistrationEmail && !isValidRegisterPassword && isMatchingPasswordAndConfirmPassword) {
+                //invalid email and password only
+                Toast.makeText(this, "Invalid Email and Password", Toast.LENGTH_LONG).show()
+            } else if (!isValidRegistrationEmail && !isValidRegisterPassword && !isMatchingPasswordAndConfirmPassword) {
+                //everything is invalid
+                Toast.makeText(this, "Invalid Email and Password also passwords don't match", Toast.LENGTH_LONG
+                ).show()
+
+            } else if (isValidRegistrationEmail && isValidRegisterPassword && !isMatchingPasswordAndConfirmPassword) {
+                //only passwords don't match
+                Toast.makeText(this, "Passwords don't match", Toast.LENGTH_LONG).show()
+
+            } else if (isValidRegistrationEmail && !isValidRegisterPassword && isMatchingPasswordAndConfirmPassword) {
+                //invalid password only
+                Toast.makeText(this, "Invalid Password", Toast.LENGTH_LONG).show()
+
+            }else if(isValidRegistrationEmail && !isValidRegisterPassword && !isMatchingPasswordAndConfirmPassword){
+                //invalid password and passwords don't match
+                Toast.makeText(this, "Invalid Password and passwords don't match", Toast.LENGTH_LONG).show()
+
+            }
+//
+//            when(isValidRegistrationEmail && isValidRegisterPassword && isMatchingPasswordAndConfirmPassword){
+//                //every entry is valid
+//                true, true, true ->   Toast.makeText(this,"Registration Successful", Toast.LENGTH_LONG).show()
+//
+//                //email invalid, password invalid, confirm password valid
+//                false,false, true -> Toast.makeText(this,"Email and password are invalid", Toast.LENGTH_LONG).show()
+//
+//                //every entry is invalid
+//                false, false, false -> Toast.makeText(this,"Everything is invalid", Toast.LENGTH_LONG).show()
+//
+//                //email invalid, password valid, confirm password valid
+//                false, true, true -> Toast.makeText(this,"Invalid email address", Toast.LENGTH_LONG).show()
+//
+//
+//                //email valid, password invalid, confirm password valid
+//                true, false, true -> Toast.makeText(this,"Invalid Password", Toast.LENGTH_LONG).show()
+//
+//
+//                //email valid,password valid, confirm password invalid
+//                true, true, false -> Toast.makeText(this,"The passwords don't match", Toast.LENGTH_LONG).show()
+//
+//
+//            }
+        }
+
+
     }
 }
